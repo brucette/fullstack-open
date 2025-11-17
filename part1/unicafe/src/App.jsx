51ede68b2523a54be61e1454d557abd6b8b6fed0
Tina Bruce <tina@fishface.se>
2025-11-17 23:11:03 +0100

    unicafe step 2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,12 +2,15 @@ import { useState } from "react";
 
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
 
-const Display = ({ good, neutral, bad }) => {
+const Display = ({ good, neutral, bad, all, average, positive }) => {
   return (
     <>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive}</p>
     </>
   );
 };
@@ -16,12 +19,20 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const total = good + neutral + bad;
 
   const handleClick = (category) => {
     if (category === "good") setGood(good + 1);
     else if (category === "neutral") setNeutral(neutral + 1)
     else setBad(bad + 1)
   };
+
+  const calculateAverage = (good, bad, total) => {
+    return total === 0 ? 0 : (good - bad) / total;
+  }
+  const calculatePositive = (good, total) => { 
+    return total === 0 ? 0 : (good / total) * 100;
+  }
 
   return (
     <>
@@ -36,7 +47,10 @@ const App = () => {
         <Display 
           good={good}
           neutral={neutral}
-          bad={bad} />
+          bad={bad}
+          all={total} 
+          average={calculateAverage(good, neutral, bad, total)}
+          positive={calculatePositive(good, total)} />
       </div>
     </>
   );
