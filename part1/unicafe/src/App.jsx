9c4887e7b795b1117f9f6b3bdb08d759feddb8bd
Tina Bruce <tina@fishface.se>
2025-11-17 23:20:16 +0100

    unicafe step 3

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,15 +2,25 @@ import { useState } from "react";
 
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
 
-const Display = ({ good, neutral, bad, all, average, positive }) => {
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad;
+
+  const calculateAverage = (good, bad, total) => {
+    return total === 0 ? 0 : (good - bad) / total;
+  }
+
+  const calculatePositive = (good, total) => { 
+    return total === 0 ? 0 : (good / total) * 100;
+  }
+
   return (
     <>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {average}</p>
-      <p>positive {positive}</p>
+      <p>all {total}</p>
+      <p>average {calculateAverage(good, bad, total)}</p>
+      <p>positive {calculatePositive(good, total)}</p>
     </>
   );
 };
@@ -19,20 +29,12 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const total = good + neutral + bad;
 
   const handleClick = (category) => {
     if (category === "good") setGood(good + 1);
     else if (category === "neutral") setNeutral(neutral + 1)
     else setBad(bad + 1)
   };
-
-  const calculateAverage = (good, bad, total) => {
-    return total === 0 ? 0 : (good - bad) / total;
-  }
-  const calculatePositive = (good, total) => { 
-    return total === 0 ? 0 : (good / total) * 100;
-  }
 
   return (
     <>
@@ -44,13 +46,10 @@ const App = () => {
       </div>
       <div>
         <h1>statistics</h1>
-        <Display 
+        <Statistics 
           good={good}
           neutral={neutral}
-          bad={bad}
-          all={total} 
-          average={calculateAverage(good, neutral, bad, total)}
-          positive={calculatePositive(good, total)} />
+          bad={bad} />
       </div>
     </>
   );
