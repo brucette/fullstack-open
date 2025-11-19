a3ea7959b7ace80b17a201a384610b254bdbd95a
Tina Bruce <tina@fishface.se>
2025-11-19 19:49:18 +0100

    phonebook step 1

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+
+function App() {
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [newName, setNewName] = useState('');
+
+  const addPerson = (e) => {
+    e.preventDefault()
+    const personObject = {
+      name: newName
+    }
+    setPersons([...persons, personObject])
+    setNewName('')
+  }
+
+  return (
+      <div>
+        <h2>Phonebook</h2>
+        <form onSubmit={addPerson}>
+          <div>
+            name: <input value={newName} onChange={(e) => setNewName(e.target.value)} />
+          </div>
+          <div>
+            <button type="submit">add</button>
+          </div>
+        </form>
+        <h2>Numbers</h2>
+        <div>
+          {persons.map(person => 
+            <p key={person.name}>{person.name}</p>
+          )}
+        </div>
+      </div>
+  );
+}
+
+export default App;
