fb55371532e9083c2db97f3ef0bde8090fb40794
Tina Bruce <tina@fishface.se>
2025-11-20 18:25:58 +0100

    phonebook step 4

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,15 +1,24 @@
 import { useState } from 'react'
 
 function App() {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas', number: '040-1234567' }]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ]);
+
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
 
   const addPerson = (e) => {
     e.preventDefault()
+
     const personObject = {
       name: newName,
-      number: newNumber
+      number: newNumber,
+      id: persons.length + 1
     }
 
     const isAlreadyAdded = persons.some(person => person.name.toLowerCase() === newName.toLowerCase())
@@ -27,9 +36,15 @@ function App() {
     return input.replace(/[^0-9+()\s-]/g, "");
   }
 
+  const namesToShow = persons.filter(person => person.name.toLowerCase().includes(searchTerm.toLowerCase()))
+
   return (
       <div>
         <h2>Phonebook</h2>
+        <div>
+          search names with<input value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+        </div>
+        <h2>Add a new person</h2>
         <form onSubmit={addPerson}>
           <div>
             name: <input value={newName} onChange={(e) => setNewName(e.target.value)} />
@@ -43,7 +58,7 @@ function App() {
         </form>
         <h2>Numbers</h2>
         <div>
-          {persons.map(person => 
+          {namesToShow.map(person => 
             <p key={person.name}>{person.name} {person.number}</p>
           )}
         </div>
