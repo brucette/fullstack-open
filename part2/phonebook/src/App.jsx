593df1d9a6579f4b74336c4f3d54fbdfbd21393f
Tina Bruce <tina@fishface.se>
2025-11-20 22:17:37 +0100

    phonebook step 6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,20 +1,21 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import axios from 'axios'
 
 function App() {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-
+  const [persons, setPersons] = useState([])
   const [name, setName] = useState('')
   const [number, setNumber] = useState('')
   const [searchTerm, setSearchTerm] = useState('')
 
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => setPersons(response.data))
+  }, [])
+  
   const addPerson = (e) => {
     e.preventDefault()
 
@@ -49,6 +50,7 @@ function App() {
         <PersonForm 
           onSubmit={addPerson} 
           name={name}
+          onNameChange={setName}
           number={number} 
           onNumberChange={setNumber}
           formatPhoneNumber={formatPhoneNumber}
