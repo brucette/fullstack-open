6af905b24d3eef2a4a43996d9a80906a23e358b6
Tina Bruce <tina@fishface.se>
2025-11-26 18:23:23 +0100

    phonebook backend steps 3 and 4

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -40,6 +40,23 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  persons = persons.filter(p => p.id !== id)
+  response.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
