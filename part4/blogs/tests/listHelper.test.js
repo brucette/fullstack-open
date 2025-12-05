05715c405e9e5a88d44f05e9e24f192d6ddf0ca5
Tina Bruce <tina@fishface.se>
2025-12-05 10:33:01 +0100

    helper functions and unit tests, step 4 and 5

--- part4/blogs/tests/listHelper.test.js
+++ part4/blogs/tests/listHelper.test.js
@@ -11,7 +11,7 @@ test('dummy returns one', () => {
 
 describe('total likes', () => {
   test('of a bigger list is calculated right', () => {
-    const listWithManyBlog = [
+    const listWithManyBlogs = [
       {
         title: "Node Streams",
         author: "Michael Reyes",
@@ -31,12 +31,13 @@ describe('total likes', () => {
         likes: 15
       }
     ]
-    const result = listHelper.totalLikes(listWithManyBlog)
+    const result = listHelper.totalLikes(listWithManyBlogs)
     assert.strictEqual(result, 35)
   })
 
   test('of empty list of blogs is zero', () => {
-    assert.strictEqual(listHelper.totalLikes([]), 0)
+    const result = listHelper.totalLikes([])
+    assert.strictEqual(result, 0)
   })
 
   test('when list has only one blog equals the likes of that', () => {
@@ -48,6 +49,144 @@ describe('total likes', () => {
         likes: 12
       }
     ]
-    assert.strictEqual(listHelper.totalLikes(listWithOneBlog), 12)
+    const result = listHelper.totalLikes(listWithOneBlog)
+    assert.strictEqual(result, 12)
+  })
+})
+
+describe('favourite blog', () => { 
+  test('of a list is calculated correctly', () => { 
+    const listWithManyBlogs = [
+      {
+        title: "Node Streams",
+        author: "Michael Reyes",
+        url: "https://example.com/blog/nodejs-streams",
+        likes: 20
+      },
+      {
+        title: "Tailwind CSS",
+        author: "Emily Carter",
+        url: "https://example.com/blog/tailwind-styling",
+        likes: 20
+      },
+      {
+        title: "REST APIs",
+        author: "Daniel Kim",
+        url: "https://example.com/blog/express-mongo-api",
+        likes: 15
+      }
+    ]
+    const favorite =  {
+      title: "Node Streams",
+      author: "Michael Reyes",
+      url: "https://example.com/blog/nodejs-streams",
+      likes: 20
+    }
+    const result = listHelper.favoriteBlog(listWithManyBlogs)
+    assert.deepStrictEqual(result, favorite)
+  })
+
+  test('of empty list returns null', () => { 
+    const result = listHelper.favoriteBlog([])
+    assert.strictEqual(result, null)
+  })
+
+  test('of a single blog returns that blog', () => {
+    const blogs = [
+      { 
+        title: "Node Streams",
+        author: "Michael Reyes",
+        url: "https://example.com/blog/nodejs-streams",
+        likes: 20
+      }
+    ]
+    const result = listHelper.favoriteBlog(blogs)
+    assert.deepStrictEqual(result, blogs[0])
+  })
+})
+
+describe('most blogs', () => { 
+  test('of a list returns the correct author and number of blogs', () => {
+    const listWithManyBlogs = [
+      {
+        title: "Node Streams",
+        author: "Michael Reyes",
+        url: "https://example.com/blog/nodejs-streams",
+        likes: 20
+      },
+      {
+        title: "Tailwind CSS",
+        author: "Michael Reyes",
+        url: "https://example.com/blog/tailwind-styling",
+        likes: 20
+      },
+      {
+        title: "REST APIs",
+        author: "Daniel Kim",
+        url: "https://example.com/blog/express-mongo-api",
+        likes: 15
+      }
+    ] 
+    const result = listHelper.mostBlogs(listWithManyBlogs)
+    assert.deepStrictEqual(result, { author: 'Michael Reyes', blogs: 2 })
+  })
+
+  test('empty list of blogs returns an empty object', () => { 
+    const result = listHelper.mostBlogs([])
+    assert.deepStrictEqual(result, {})  
+  })
+
+  test('list of one blog returns that author', () => {
+    const result = listHelper.mostBlogs([{
+      title: "Node Streams",
+      author: "Michael Reyes",
+      url: "https://example.com/blog/nodejs-streams",
+      likes: 20
+    }])
+
+    assert.deepStrictEqual(result, { author: "Michael Reyes", blogs: 1 })
+  })
+})
+
+describe('most likes', () => { 
+  test('of a list returns the correct author and number of likes', () => {
+    const listWithManyBlogs = [
+      {
+        title: "Node Streams",
+        author: "Michael Reyes",
+        url: "https://example.com/blog/nodejs-streams",
+        likes: 20
+      },
+      {
+        title: "Tailwind CSS",
+        author: "Michael Reyes",
+        url: "https://example.com/blog/tailwind-styling",
+        likes: 20
+      },
+      {
+        title: "REST APIs",
+        author: "Daniel Kim",
+        url: "https://example.com/blog/express-mongo-api",
+        likes: 15
+      }
+    ]
+    const result = listHelper.mostLikes(listWithManyBlogs)
+    assert.deepStrictEqual(result, { author: "Michael Reyes", likes: 40 })
+  })
+
+  test('empty list of blogs returns an empty object', () => { 
+    const result = listHelper.mostLikes([])
+    assert.deepStrictEqual(result, {})  
+  })
+
+  test('list of one blog returns that author', () => {
+    const result = listHelper.mostLikes([{
+      title: "Node Streams",
+      author: "Michael Reyes",
+      url: "https://example.com/blog/nodejs-streams",
+      likes: 20
+    }])
+
+    assert.deepStrictEqual(result, { author: "Michael Reyes", likes: 20 })
   })
 })
--- part4/blogs/utils/list_helper.js
+++ part4/blogs/utils/list_helper.js
@@ -6,13 +6,53 @@ const totalLikes = (blogs) => {
   const reducer = (sum, item) => {
     return sum + item.likes
   }
-
   return blogs.length === 0 
-  ?  0
+  ? 0
   : blogs.reduce(reducer, 0)
+}
+
+const favoriteBlog = (blogs) => { 
+  return blogs.length === 0 
+    ? null
+    : blogs.reduce((max, blog) => {
+      return blog.likes > max.likes ? blog : max
+    }, blogs[0]) 
+}
+
+const mostBlogs = (blogs) => { 
+  if (blogs.length === 0) return {}
+
+  const counts = blogs.reduce((counts, blog) => {
+    counts[blog.author] = (counts[blog.author] || 0) + 1
+    return counts
+  }, {})
+
+  const [author, blogsCount] = Object.entries(counts).reduce((max, entry) => {
+    return entry[1] > max[1] ? entry : max
+  })
+
+  return { author, blogs: blogsCount }
+}
+
+const mostLikes = (blogs) => {
+  if (blogs.length === 0) return {}
+
+  const counts = blogs.reduce((counts, blog) => {
+    counts[blog.author] = (counts[blog.author] || 0) + blog.likes
+    return counts
+  }, {})
+  
+  const [author, likes] = Object.entries(counts).reduce((max, entry) => {
+    return entry[1] > max[1] ? entry : max
+  })
+
+  return { author, likes }
 }
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
 }
