d9a5ea8806f9dc37a1b4512c70956c075b65051c
Tina Bruce <tina@fishface.se>
2025-12-19 16:31:20 +0100

    4.19: Blog List Expansion - make creating blog possible only for auth user

--- part4/blogs/controllers/blogs.js
+++ part4/blogs/controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -8,11 +17,15 @@ blogsRouter.get('/', async (request, response) => {
 })
 
 blogsRouter.post('/', async (request, response) => {
-  // const { title, author, url, likes, userId } = request.body
   const { title, author, url, likes } = request.body
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
 
-  const user = await User.findOne({})
-  // const user = await User.findById(userId)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+
+  const user = await User.findById(decodedToken.id)
+
   if (!user) {
     return response.status(400).json({ error: 'userId missing or not valid' })
   }
@@ -22,7 +35,6 @@ blogsRouter.post('/', async (request, response) => {
     author,
     url,
     likes,
-    // userId
     user: user._id,
   })
 
