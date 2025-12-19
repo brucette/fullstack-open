6e19dc5ab8088f45d011680a701c0b4f7bc43103
Tina Bruce <tina@fishface.se>
2025-12-19 17:18:14 +0100

    4.20: Blog List Expansion - refactor token extraction to middleware

--- part4/blogs/utils/middleware.js
+++ part4/blogs/utils/middleware.js
@@ -16,7 +16,19 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+
+  if (authorization && authorization.startsWith('Bearer ')) {
+    request.token = authorization.replace('Bearer ', '')
+  } else {
+    request.token = null
+  }
+  next()
+}
+
 module.exports = {
   unknownEndpoint,
-  errorHandler
+  errorHandler,
+  tokenExtractor
 }
