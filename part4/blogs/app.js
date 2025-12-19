1a4c70bb3dfa34cc4dd6b7384e027316c764128f
Tina Bruce <tina@fishface.se>
2025-12-19 10:25:56 +0100

    4.18: Blog List Expansion - implement token-based authentication

--- part4/blogs/app.js
+++ part4/blogs/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
 const middleware = require('./utils/middleware')
@@ -13,6 +14,7 @@ app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
