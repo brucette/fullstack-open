769be5a8d87840a5b7a7317e232d0bd1c475247f
Tina Bruce <tina@fishface.se>
2026-01-29 21:39:31 +0100

    5.15: Blog List Tests - clicking like twice calls event handler twice

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -10,15 +10,16 @@ const blog = {
 }
 
 describe('<Blog />', () => {
-  beforeEach(() => {
-    render(<Blog blog={blog} />)
-  })
+  // beforeEach(() => {
+  // })
 
   test('renders title and author, but not URL or likes by default', () => {
+    render(<Blog blog={blog} />)
+
     const title = screen.getByText('Find the promise land3', { exact: false })
     const author = screen.getByText('Holy Landis')
-    const url = screen.getByText('https://example.com/blog/promise-land3')
-    const likes = screen.getByText('likes', { exact: false })
+    const url = screen.queryByText('https://example.com/blog/promise-land3')
+    const likes = screen.queryByText('likes', { exact: false })
 
     expect(title).toBeVisible()
     expect(author).toBeVisible()
@@ -27,6 +28,8 @@ describe('<Blog />', () => {
   })
 
   test('URL and likes rendered when view button clicked', async () => {
+    render(<Blog blog={blog} />)
+
     const viewButton = screen.getByText('view')
     const user = userEvent.setup()
     await user.click(viewButton)
@@ -36,5 +39,20 @@ describe('<Blog />', () => {
 
     expect(url).toBeVisible()
     expect(likes).toBeVisible()
+  })
+
+  test('clicking like twice calls event handler twice', async () => {
+    const likeBlog = vi.fn()
+    render(<Blog blog={blog} likeBlog={likeBlog}/>)
+
+    const viewButton = screen.getByText('view')
+    const user = userEvent.setup()
+    await user.click(viewButton)
+
+    const likeButton = screen.getByText('like')
+    await user.click(likeButton)
+    await user.click(likeButton)
+
+    expect(likeBlog.mock.calls).toHaveLength(2)
   })
 })
